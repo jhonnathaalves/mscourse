644fd744d75e0ef6ddfc44abe765b68a7c770e28
unknown <jhonnatha16011987@yahoo.com.br>
2022-01-07 21:50:06 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/jhonnatha/cursomc/services/CategoriaService.java
+++ src/main/java/com/jhonnatha/cursomc/services/CategoriaService.java
@@ -19,6 +19,11 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new com.jhonnatha.cursomc.services.exceptions.ObjectNotFoundException( 
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 		} 
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 
 
 }
