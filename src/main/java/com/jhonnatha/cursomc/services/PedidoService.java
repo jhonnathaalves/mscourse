2da3fd247913c46b631105eaca1e49f63642607e
unknown <jhonnatha16011987@yahoo.com.br>
2022-01-19 22:55:28 -0300

    Email HTML

--- src/main/java/com/jhonnatha/cursomc/services/PedidoService.java
+++ src/main/java/com/jhonnatha/cursomc/services/PedidoService.java
@@ -65,7 +65,7 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItems());
-		emailService.sendOrderConfirmationEmail(obj);
+		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 
