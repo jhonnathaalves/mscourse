91c27fe27855f06da278556a515b6ab7e39922f8
unknown <jhonnatha16011987@yahoo.com.br>
2022-01-20 22:09:35 -0300

    Adicionando senha a Cliente

--- src/main/java/com/jhonnatha/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/jhonnatha/cursomc/dto/ClienteNewDTO.java
@@ -20,7 +20,11 @@ public class ClienteNewDTO implements Serializable {
 	
 	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	private String senha;
 
 	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
@@ -145,6 +149,14 @@ public class ClienteNewDTO implements Serializable {
 
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 
 }
--- src/main/java/com/jhonnatha/cursomc/services/DBService.java
+++ src/main/java/com/jhonnatha/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.jhonnatha.cursomc.domain.Categoria;
@@ -34,6 +35,9 @@ import com.jhonnatha.cursomc.repositories.ProdutoRepository;
 public class DBService {
 	
 	@Autowired
+	private BCryptPasswordEncoder pe;
+	
+	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
@@ -51,6 +55,8 @@ public class DBService {
 	private PagamentoRepository pagamentoRepository;
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	
 
 	public void instantiateTestDatabase() throws ParseException {
 
@@ -110,7 +116,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "jhonnathaicm@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "jhonnathaicm@gmail.com", "36378912377", TipoCliente.PESSOAFISICA,pe.encode("123"));
 
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
